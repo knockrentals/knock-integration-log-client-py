fa3cf626921bff545d7f9ab4aca70fa4d599e06f
Kiefer Hagin <kieferhagib@gmail.com>
2018-09-12 17:02:22 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='knockintegrationlogclient',
+    version='1.0',
+    description='Knock integration logging client',
+    author='Knock, Inc.',
+    packages=['knock_integration_log_client'])
