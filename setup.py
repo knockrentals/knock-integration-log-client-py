aa533c3ec7855f47110bba25dbc8eaf8f1b10486
kieferhagin <kieferhagin@gmail.com>
2018-09-24 12:25:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='knockintegrationlogclient',
-    version='1.0',
+    version='1.1',
     description='Knock integration logging client',
     author='Knock, Inc.',
     packages=['knock_integration_log_client'])
